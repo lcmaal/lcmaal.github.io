0f0a052b8ad9ab319fbcb50406575d99f4da8183
annamlthomson <annamlthomson@gmail.com>
2016-11-13 21:20:08 +0000

    deleted test.js
    already in js/test.js

--- test.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-	$('img.collection').mouseenter(function() {
-		$('this').fadeTo('fast', 1);
-	});
-	$('img.collection').mouseleave(function() {
-		$('this').fadeTo('fast', 0.5);
-	});
-});
