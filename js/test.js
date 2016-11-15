fc205e0e49c5bdd772361d0831c3802632acbde0
annamlthomson <annathomson@Anna-Thomsons-MacBook-Pro.local>
2016-11-15 00:39:03 +0000

    some changes
    Changed navbar to push to front, added some jquery to change opacity
    when hovering, added divs for poems and message at bottom

--- js/test.js
+++ js/test.js
@@ -1,8 +1,56 @@
 $(document).ready(function() {
-	$('img.collection').mouseenter(function() {
-		$('this').fadeTo('fast', 1);
+	$('img#richardpic').mouseenter(function() {
+		$('img#richardpic').fadeTo('fast', 1);
 	});
-	$('img.collection').mouseleave(function() {
-		$('this').fadeTo('fast', 0.5);
+	$('img#richardpic').mouseleave(function() {
+		$('img#richardpic').fadeTo('fast', 0.65);
+	});
+	$('img#georgiapic').mouseenter(function() {
+		$('img#georgiapic').fadeTo('fast', 1);
+	});
+	$('img#georgiapic').mouseleave(function() {
+		$('img#georgiapic').fadeTo('fast', 0.65);
+	});
+	$('img#eddiepic').mouseenter(function() {
+		$('img#eddiepic').fadeTo('fast', 1);
+	});
+	$('img#eddiepic').mouseleave(function() {
+		$('img#eddiepic').fadeTo('fast', 0.65);
+	});
+	$('img#mariapic').mouseenter(function() {
+		$('img#mariapic').fadeTo('fast', 1);
+	});
+	$('img#mariapic').mouseleave(function() {
+		$('img#mariapic').fadeTo('fast', 0.65);
+	});
+	$('img#abbypic').mouseenter(function() {
+		$('img#abbypic').fadeTo('fast', 1);
+	});
+	$('img#abbypic').mouseleave(function() {
+		$('img#abbypic').fadeTo('fast', 0.65);
+	});
+	$('img#joypic').mouseenter(function() {
+		$('img#joypic').fadeTo('fast', 1);
+	});
+	$('img#joypic').mouseleave(function() {
+		$('img#joypic').fadeTo('fast', 0.65);
+	});
+	$('img#rosepic').mouseenter(function() {
+		$('img#rosepic').fadeTo('fast', 1);
+	});
+	$('img#rosepic').mouseleave(function() {
+		$('img#rosepic').fadeTo('fast', 0.65);
+	});
+	$('img#lolapic').mouseenter(function() {
+		$('img#lolapic').fadeTo('fast', 1);
+	});
+	$('img#lolapic').mouseleave(function() {
+		$('img#lolapic').fadeTo('fast', 0.65);
+	});
+	$('img#addnewpic').mouseenter(function() {
+		$('img#addnewpic').fadeTo('fast', 1);
+	});
+	$('img#addnewpic').mouseleave(function() {
+		$('img#addnewpic').fadeTo('fast', 0.65);
 	});
 });
